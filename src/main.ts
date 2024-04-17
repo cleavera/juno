985a10fff35d6662aee0ff9e0e6582bbbf9db6b3
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-04-17 19:47:23 +0100

    Convert to PWA

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
+import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 import { AppComponent } from './app/components/app/app.component';
+import { ComparisonFactory } from './app/services/comparison.factory';
 import { HeadToHeadSerialiserService } from './app/services/head-to-head-serialiser.service';
 import { LeagueSerialiserService } from './app/services/league-serialiser.service';
 import { LeagueFactory } from './app/services/league.factory';
 import { PersistenceService } from './app/services/persistence.service';
 import { RoundSerialiserService } from './app/services/round-serialiser.service';
 import { RoundFactory } from './app/services/round.factory';
-import { ComparisonFactory } from './app/services/comparison.factory';
-
 bootstrapApplication(AppComponent, {
   providers: [
     ComparisonFactory,
@@ -16,7 +17,11 @@ bootstrapApplication(AppComponent, {
     LeagueSerialiserService,
     PersistenceService,
     RoundFactory,
-    RoundSerialiserService
+    RoundSerialiserService,
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 })
   .catch((err) => console.error(err));
